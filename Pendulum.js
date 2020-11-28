13b6e6445597af6b3fca8ff66d62a1a484ac5a62
debopom-dey <71516417+debopom-dey@users.noreply.github.com>
2020-11-28 16:10:30 +0530

    Add files via upload

---
+++ Pendulum.js
@@ -0,0 +1,50 @@
+class Pendulum{
+    constructor(x,y,color){
+       var options={
+       isStatic:false,
+       restitution:1,
+       friction:0,
+       frictionAir:0.0,
+       slop:1,
+       inertia:Infinity
+  
+      }
+      this.body= Bodies.rectangle(x,y,40,40,options)
+      this.x=x;
+      this.y=y;
+      this.color=color;
+      //bobDiameter=40;
+     
+      World.add(world,this.body)
+  
+    }
+  
+    display(){
+      var angle= this.body.angle
+        var pos= this.body.position;
+        push()
+        translate(pos.x,pos.y);
+        rotate(angle);
+        noStroke()
+        fill(this.color);
+        ellipse(0,0,60,60);
+        pop()
+    }
+  
+  
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
---
+++ sketch.js
@@ -0,0 +1,65 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Mouse = Matter.Mouse;
+const Constraint = Matter.Constraint;
+const MouseConstraint = Matter.Constraint;
+var canvas,world,engine,mConstraint;
+var bob1,bob2,bob3,bob4,bob5,roof,rope,bobDiameter
+function preload()
+{
+	
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth / 2, windowHeight / 1.5);
+  engine = Engine.create();
+  world = engine.world;
+
+  let canvasmouse = Mouse.create(canvas.elt);
+  canvasmouse.pixelRatio = pixelDensity();
+  let options = {
+    mouse: canvasmouse
+  };
+  mConstraint = MouseConstraint.create(engine, options);
+  World.add(world, mConstraint);
+
+  bob1= new Pendulum(150,250,255)
+  rope1= new Rope(bob1.body,{x:150,y:250})
+  bob2= new Pendulum(210,250,0)
+  rope2= new Rope(bob2.body,{x:210,y:250})
+  bob3= new Pendulum(270,250,255)
+  rope3= new Rope(bob3.body,{x:270,y:250})
+  bob4= new Pendulum(330,250,0)
+  rope4= new Rope(bob4.body,{x:330,y:250})
+  bob5= new Pendulum(390,250,255)
+  rope5= new Rope(bob5.body,{x:390,y:250})
+	//Engine.run(engine);
+  
+}
+
+
+function draw() {
+  background(210);
+  Engine.update(engine);
+  
+  bob1.display();
+  rope1.display();
+  bob2.display();
+  rope2.display();
+  bob3.display();
+  rope3.display();
+  bob4.display();
+  rope4.display();
+  bob5.display();
+  rope5.display();
+  drawSprites();
+ 
+}
+function mouseDragged(){
+  Matter.Body.setPosition(bob1.body,{x:mouseX,y:mouseY})
+}
+
+
